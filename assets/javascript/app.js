18d901059f71fe037750194a392394f4af9c99c1
Fred Astaire <lancebd@thegodardgroup.com>
2019-03-04 01:03:00 -0500

    Added comments re functions and functionability

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,24 +19,22 @@ var searchText = ""; // text from input box that will eventually become search t
 var topics = ["Paris", "London", "Rome", "New York", "Hong Kong", "Tokyo"];
 var gifArray = []; // will hold all the JSON data returned from the AJAX get
 var gifSearch = ""; // name of searchText used in query URL and to push to DOM
-var url = ""; 
-var rating = "";
 
 renderButtons();
 getGIFs();
 
-$(document).ready(function () {
+$(document).ready(function () { // sets everything in motion
     $("#add-user").on("click", function (event) {
         event.preventDefault();
-        searchText = $("#searchText").val().trim();
-        topics.push(searchText);
+        searchText = $("#searchText").val().trim(); // grabs text from input field
+        topics.push(searchText); // pushes new search terms into the array used for creating buttons
         renderButtons();
         getGIFs();
     })
 });
-function renderButtons() {
-    $("#button-space").empty();
-    for (var i = 0; i < topics.length; i++) {
+function renderButtons() { // this function turns text from the input field into buttons that will be used for GIF searches
+    $("#button-space").empty(); // empties out the div so buttons are not duplicated
+    for (var i = 0; i < topics.length; i++) { // loops through the topics array to create buttons based on search terms; always rewrites all buttons
         var a = $("<button>");
         a.addClass("button-text");
         a.addClass("btn-primary");
@@ -45,23 +43,21 @@ function renderButtons() {
         $("#button-space").append(a);
     }
 }
-function getGIFs() {
-    $(".button-text").on("click", function () {
-        $("#gif-space").empty();
-        gifArray = [];
-        var gifSearch = $(this).attr("data-name");
+function getGIFs() { // get GIF data from the Giphy API; push selected items into new array that will be used to dynamically create GIFs on DOM
+    $(".button-text").on("click", function () { // listens for clicks on the search text buttons 
+        $("#gif-space").empty(); // clears any GIFs out of the gif space
+        var gifSearch = $(this).attr("data-name"); // grabs the text from the button
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&q=" + gifSearch + "&limit=10&offset=0&rating=G&lang=en";
-        $.ajax({
+        $.ajax({ // runs the AJAX get
             url: queryURL,
             method: "GET"
         })
-            .then(function (response) {
-                console.log(response);
+            .then(function (response) { // this function pushes three distinct pieces of data from the API into the array from which the GIFs will be constructed
                 for (var j = 0; j < 10; j++) {
-                    var fixedURL = response.data[j].images.fixed_width_still.url;
-                    var movingURL = response.data[j].images.fixed_width.url
-                    var gifRating = response.data[j].rating
-                    gifArray.push({
+                    var fixedURL = response.data[j].images.fixed_width_still.url; // the still image
+                    var movingURL = response.data[j].images.fixed_width.url // the animated image
+                    var gifRating = response.data[j].rating // the rating of the GIF
+                    gifArray.push({ // pushes data from response into the array; sets them up as objects so each individual data item can be pushed into the div for each GIF
                         fixed_url: fixedURL,
                         gif_url: movingURL,
                         rating: gifRating
@@ -71,7 +67,7 @@ function getGIFs() {
             })
     })
 }
-function gifsDOM() {
+function gifsDOM() { // pushes the GIFs to the DOM. Assigns numerous attributes to the image tag so that animation can be turned on and off. Creates each individual div virtually then posts entire div to DOM
     $("#gifs-title").text("Here are your GIFs. Enjoy!");
     for (var x = 0; x < gifArray.length; x++) {
         var gifsDiv = $("<div class='buttonGIF'>");
@@ -89,7 +85,7 @@ function gifsDOM() {
     }
     animateGIFs();
 }
-function animateGIFs() {
+function animateGIFs() { // turns animation on and off 
     $(".gif").on("click", function () {
         var state = $(this).attr("data-state");
         if (state === "still") {
