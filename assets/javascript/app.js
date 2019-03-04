cb165f3fc9979942b55598061b30a5b740bad979
Fred Astaire <lancebd@thegodardgroup.com>
2019-03-04 10:44:33 -0500

    style fixes and eliminate extra array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var searchText = ""; // text from input box that will eventually become search term on Giphy
-var topics = ["Paris", "London", "Rome", "New York", "Hong Kong", "Tokyo"];
+var topics = ["Paris", "London", "Rome", "New York", "Hong Kong", "Tokyo", "Mexico City", "Delhi"];
 var gifArray = []; // will hold all the JSON data returned from the AJAX get
 var gifSearch = ""; // name of searchText used in query URL and to push to DOM
 
@@ -13,6 +13,7 @@ $(document).ready(function () { // sets everything in motion
         topics.push(searchText); // pushes new search terms into the array used for creating buttons
         renderButtons();
         getGIFs();
+        animateGIFs();
     })
 });
 function renderButtons() { // this function turns text from the input field into buttons that will be used for GIF searches
@@ -28,7 +29,6 @@ function renderButtons() { // this function turns text from the input field into
 }
 function getGIFs() { // get GIF data from the Giphy API; push selected items into new array that will be used to dynamically create GIFs on DOM
     $(".button-text").on("click", function () { // listens for clicks on the search text buttons 
-        $("#gif-space").empty(); // clears any GIFs out of the gif space
         gifSearch = $(this).attr("data-name"); // grabs the text from the button
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&q=" + gifSearch + "&limit=10&offset=0&rating=G&lang=en";
         $.ajax({ // runs the AJAX get
@@ -36,38 +36,26 @@ function getGIFs() { // get GIF data from the Giphy API; push selected items int
             method: "GET"
         })
             .then(function (response) { // this function pushes three distinct pieces of data from the API into the array from which the GIFs will be constructed
+                $(".buttonGIF").empty(); // clears any GIFs out of the gif space
+                $("#gifs-title").text("These " + gifSearch + " GIFs are the next best thing to being there. Enjoy!");
                 for (var j = 0; j < 10; j++) {
-                    var fixedURL = response.data[j].images.fixed_width_still.url; // the still image
-                    var movingURL = response.data[j].images.fixed_width.url // the animated image
-                    var gifRating = response.data[j].rating // the rating of the GIF
-                    gifArray.push({ // pushes data from response into the array; sets them up as objects so each individual data item can be pushed into the div for each GIF
-                        fixed_url: fixedURL,
-                        gif_url: movingURL,
-                        rating: gifRating
-                    });
-                }
-                gifsDOM();
-            })
+                    var gifsDiv = $("<span class='buttonGIF'>");
+                    var image = $("<img>").attr({
+                        "src": response.data[j].images.fixed_width_still.url,
+                        "data-still": response.data[j].images.fixed_width_still.url,
+                        "data-animate": response.data[j].images.fixed_width.url,
+                        "data-state": "still",
+                        "class": "gif"
+                    })
+                    gifsDiv.append(image);
+                    var pZero = $("<span>").text("Rating: " + response.data[j].rating);
+                    gifsDiv.append(pZero);
+                    $("#gif-space").prepend(gifsDiv);
+                };
+                animateGIFs();
+            });
     })
-}
-function gifsDOM() { // pushes the GIFs to the DOM. Assigns numerous attributes to the image tag so that animation can be turned on and off. Creates each individual div virtually then posts entire div to DOM
-    $("#gifs-title").text("These " + gifSearch + " GIFs are the next best thing to being there. Enjoy!");
-    for (var x = 0; x < gifArray.length; x++) {
-        var gifsDiv = $("<span class='buttonGIF'>");
-        var image = $("<img>").attr({
-            "src": gifArray[x].fixed_url,
-            "data-still": gifArray[x].fixed_url,
-            "data-animate": gifArray[x].gif_url,
-            "data-state": "still",
-            "class": "gif"
-        })
-            gifsDiv.append(image);
-            var pZero = $("<span>").text("Rating: " + gifArray[x].rating);
-            gifsDiv.append(pZero);
-            $("#gif-space").prepend(gifsDiv);
-    }
-    animateGIFs();
-}
+};
 function animateGIFs() { // turns animation on and off 
     $(".gif").on("click", function () {
         var state = $(this).attr("data-state");
