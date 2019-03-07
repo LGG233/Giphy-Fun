43eb8d41bdd6fbb090a075a6cdae7bda01ebdbe4
Fred Astaire <lancebd@thegodardgroup.com>
2019-03-07 08:08:36 -0500

    fixed horizontal alignment of gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,19 +26,19 @@ function renderButtons() { // this function turns text from the input field into
         $("#button-space").append(a);
     }
 }
-function getGIFs() { // get GIF data from the Giphy API; push selected items into new array that will be used to dynamically create GIFs on DOM
+function getGIFs() { // get GIF data from the Giphy API; push selected items into dynamic divs then put into the DOM
     $(".button-text").on("click", function () { // listens for clicks on the search text buttons 
-        gifSearch = $(this).attr("data-name"); // grabs the text from the button
+        gifSearch = $(this).attr("data-name"); // grabs text from the button
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=wxu2jPOXlj0bnJJQl4bDrvxy4Og5iADT&q=" + gifSearch + "&limit=10&offset=0&rating=G&lang=en";
         $.ajax({ // runs the AJAX get
             url: queryURL,
             method: "GET"
         })
-            .then(function (response) { // this function pushes three distinct pieces of data from the API into the array from which the GIFs will be constructed
-                $(".buttonGIF").empty(); // clears any GIFs out of the gif space
+            .then(function (response) { // this function pushes three distinct pieces of data from the API into the div before writing the new div to the DOM
+                $("#gif-space").empty(); // clears any GIFs out of the gif space
                 $("#gifs-title").text("These " + gifSearch + " GIFs are the next best thing to being there. Enjoy!");
                 for (var j = 0; j < 10; j++) {
-                    var gifsDiv = $("<div class='buttonGIF'>");
+                    var gifsDiv = $("<div class='searchedGIF'>");
                     var image = $("<img>").attr({
                         "src": response.data[j].images.fixed_width_still.url,
                         "data-still": response.data[j].images.fixed_width_still.url,
